736790893ab7ce1826c27cd6945854019a87d887
yunishimura0716 <yunishimura0716@gmail.com>
2020-10-04 10:44:08 -0700

    added json server and http request class

--- src/app.js
+++ src/app.js
@@ -17,3 +17,23 @@
 // console.log(mod.person.name);
 // console.log(mod.sayHello());
 // console.log(greeting);
+
+import { http } from './http';
+import { ui } from './ui';
+
+// get posts on DOM load
+document.addEventListener('DOMContentLoaded', getPosts);
+// listen fro add post
+document.querySelector('.post-submit').addEventListener('click', addPost);
+
+// get post
+function getPosts() {
+    http.get('http://localhost:3000/posts')
+        .then(data => ui.showPosts(data))
+        .catch(err => console.log(err));
+}
+// add post
+function submitPost() {
+    const title = document.querySelector('#title').value;
+    const body = document.querySelector('#body').value;
+} 
