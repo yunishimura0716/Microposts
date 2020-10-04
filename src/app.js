d642f3a3905ec5465b4c24391fcd3c918fec0333
yunishimura0716 <yunishimura0716@gmail.com>
2020-10-03 23:49:34 -0700

    set up webpack and modified index html!

---
+++ src/app.js
@@ -0,0 +1,19 @@
+// const getData = async (url) => {
+//   const response = await fetch(url);
+//   const result = await response.json();
+//   console.log(result);
+// };
+
+// getData('https://jsonplaceholder.typicode.com/posts');
+
+// common js module syntax
+// const person = require('./mymodule');
+
+// ES2015 module
+// import { person, sayHello } from './mymodule2';
+// import * as mod from './mymodule2';
+// import greeting from './mymodule2';
+
+// console.log(mod.person.name);
+// console.log(mod.sayHello());
+// console.log(greeting);
